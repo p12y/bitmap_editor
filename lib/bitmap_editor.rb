35210e9ce60846015260c1d997007d0dfc7695e9
Peter Tyldesley <ptylde@gmail.com>
2017-07-23 19:53:58 +0100

    Add color_horizontal_segment regex to switch statement

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -11,6 +11,8 @@ class BitmapEditor
       case line
       when /I\s(\d+)\s(\d+)/
         @bitmap = create_bitmap($1.to_i, $2.to_i)
+      when /H\s(\d+)\s(\d+)\s(\d+)\s([A-Z])/
+        @bitmap = color_horizontal_segment($1.to_i, $2.to_i, $3.to_i, $4)
       when "S"
         show
       else
@@ -25,6 +27,10 @@ class BitmapEditor
 
   def show
     @bitmap.render
+  end
+
+  def color_horizontal_segment(c1, c2, r, c)
+    @bitmap.color_horizontal_segment(c1, c2, r, c)
   end
 
 end
