48005aab7b86b2030bbfd3879e6896621b31e667
Peter Tyldesley <ptylde@gmail.com>
2017-07-23 19:44:35 +0100

    Add spec for color_horizontal_segment method

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -3,18 +3,27 @@ require_relative '../lib/bitmap_editor'
 describe BitmapEditor do
   before :each do
     @bitmap_editor = BitmapEditor.new
+    @bitmap = @bitmap_editor.create_bitmap(2, 2)
   end
 
   it "creates bitmap" do
-    @bitmap = @bitmap_editor.create_bitmap(2, 2)
     expect(@bitmap).to be_instance_of Bitmap
   end
 
   it "shows bitmap" do
-    @bitmap = @bitmap_editor.create_bitmap(2, 2)
     expect do
       @bitmap_editor.show
     end.to output("OO\nOO\n").to_stdout
   end
+
+  it "colors horizontal segment" do
+    @bitmap = @bitmap_editor.create_bitmap(4, 4)
+    @bitmap = @bitmap_editor.color_horizontal_segment(2, 3, 1, "C")
+    expect do
+      @bitmap_editor.show
+    end.to output("OCCO\nOOOO\n\OOOO\n").to_stdout
+  end
+
+
   
 end
