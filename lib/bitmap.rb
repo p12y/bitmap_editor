871cd858db724112199e860e5e9c2229f41925a1
Peter Tyldesley <ptylde@gmail.com>
2017-07-25 10:48:23 +0100

    Add private methods

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -27,18 +27,14 @@ class Bitmap
   end
 
   def color_horizontal_segment(**keyword_args)
-    keyword_args[:coords].each do |k, v|
-      instance_variable_set "@#{k}", v - 1
-    end
+    set_vars(keyword_args)
     check_in_bounds(cols: [@col1 + 1, @col2 + 1], row: @row + 1)
     row = @array[@row]
     row[@col1..@col2] = row[@col1..@col2].map {|char| char = keyword_args[:color]}
   end
 
   def color_vertical_segment(**keyword_args)
-    keyword_args[:coords].each do |k, v|
-      instance_variable_set "@#{k}", v - 1
-    end
+    set_vars(keyword_args)
     check_in_bounds(col: @col + 1, rows: [@row1 + 1, @row2 + 1])
     @array.slice(@row1, @row2).each do |arr|
       arr[@col] = keyword_args[:color]
@@ -52,38 +48,42 @@ class Bitmap
   end
 
   def color(**keyword_args)
-    keyword_args[:coords].each do |k, v|
-      instance_variable_set "@#{k}", v - 1
-    end
+    set_vars(keyword_args)
     check_in_bounds(col: @col + 1, row: @row + 1)
     @array[@row][@col] = keyword_args[:color]
   end
 
-  def check_in_bounds(**keyword_arguments)
-    keyword_arguments.each do |k, v|
-      case k
-      when :col
-        validate_coord(v, @width, :column)
-      when :row
-        validate_coord(v, @length, :row)
-      when :cols
-        keyword_arguments[:cols].each do |col|
-          validate_coord(col, @width, :column)
-        end
-      when :rows
-        keyword_arguments[:rows].each do |row|
-          validate_coord(row, @length, :row)
+  private
+
+    def set_vars(args)
+      args[:coords].each do |k, v|
+        instance_variable_set "@#{k}", v - 1
+      end
+    end
+
+    def check_in_bounds(**keyword_arguments)
+      keyword_arguments.each do |k, v|
+        case k
+        when :col
+          validate_coord(v, @width, :column)
+        when :row
+          validate_coord(v, @length, :row)
+        when :cols
+          keyword_arguments[:cols].each do |col|
+            validate_coord(col, @width, :column)
+          end
+        when :rows
+          keyword_arguments[:rows].each do |row|
+            validate_coord(row, @length, :row)
+          end
         end
       end
     end
-  end
 
-  def validate_coord(coord, dimension, type)
-    if !(1..dimension).include?(coord)
-      abort "#{type.to_s.capitalize} coordinates must be between 1 and #{dimension}"
+    def validate_coord(coord, dimension, type)
+      if !(1..dimension).include?(coord)
+        abort "#{type.to_s.capitalize} coordinates must be between 1 and #{dimension}"
+      end
     end
-  end
-
-
 
 end
